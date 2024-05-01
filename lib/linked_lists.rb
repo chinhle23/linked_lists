24621f5f17c9f4af232aec0f5facc469b0091e84
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-05-01 04:29:00 -0400

    Define #size, #head, & #tail methods

--- lib/linked_lists.rb
+++ lib/linked_lists.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class LinkedList
-  attr_accessor :name
+  attr_accessor :head, :tail, :size
 
   def initialize
     @head = nil
     @tail = nil
+    @size = 0
   end
 
   def append(node)
@@ -16,6 +17,7 @@ class LinkedList
       @tail.next_node = node
       @tail = node
     end
+    @size += 1
   end
 
   def prepend(node)
@@ -26,11 +28,12 @@ class LinkedList
       node.next_node = @head
       @head = node
     end
+    @size += 1
   end
 end
 
 class Node
-  attr_accessor :value, :next_node
+  attr_accessor :value, :next_node, :count
 
   def initialize(value)
     @value = value
@@ -45,3 +48,6 @@ linked_list.append(Node.new(3))
 linked_list.prepend(Node.new(0))
 
 p linked_list
+p linked_list.head
+p linked_list.tail
+p linked_list.size
